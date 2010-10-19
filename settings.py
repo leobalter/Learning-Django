970b4edd832bbd38aff25060ea37456a3b20af00
Leonardo Balter <leonardo.balter@gmail.com>
2010-10-19 15:38:17 -0200

    agora sim corrigi o db para ficar como o da apostila

--- settings.py
+++ settings.py
@@ -9,7 +9,6 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
-	('leobalter', leonardo.balter@gmail.com)
 )
 
 MANAGERS = ADMINS
@@ -17,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlitefile',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_DIR, 'database.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
