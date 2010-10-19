c10c7806841efe98f70fef1e4664d493f0fcdf45
Leonardo Balter <leonardo.balter@gmail.com>
2010-10-19 15:27:33 -0200

    colocando import os no settings.py para ajustar questao dos templates

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 # Django settings for blog project.
+import os
+
+PROJECT_DIR = os.path.dirname(__file__)
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -45,17 +49,17 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '2id)6lqy18l2+un)2i!3i1sg81e%+wvk1u$lfydz-dbn7o)ugf'
@@ -81,6 +85,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	os.path.join(PROJECT_DIR, 'templates')
 )
 
 INSTALLED_APPS = (
