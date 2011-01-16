d09f8b99563f469125c13bf5e7a231476b0584cd
Leo Balter <leonardo.balter@gmail.com>
2011-01-16 02:19:25 -0200

    Agora com boilerplate, inicio do template e servindo media e static para django 1.3

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,3 +10,11 @@ urlpatterns = patterns('',
 	(r'^posts/', include('posts.urls')),
 	(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
